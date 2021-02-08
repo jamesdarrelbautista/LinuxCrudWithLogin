0200ca8b3d8fd39d120da7cbb3e8edeee27679ae
James Darrel Bautista <jamesdarrel.bautista@g.msuiit.edu.ph>
2021-02-08 23:53:36 +0800

    added echo that you can just delete in line76

--- application/views/login_view.php
+++ application/views/login_view.php
@@ -66,23 +66,23 @@
       </div>
         <?php  
       
-        echo form_open('Main/login_action');  
+        // echo form_open('Main/login_action');  
       
-        echo validation_errors();  
+        // echo validation_errors();  
         
-        echo "<p>Username: ";  
-        echo form_input('username', $this->input->post('username'));
-        echo "</p>";  
+        // echo "<p>Username: ";  
+        // echo form_input('username', $this->input->post('username'));
+        // echo "</p>";  
+                    //  echo
+        // echo "<p>Password: ";  
+        // echo form_password('password');  
+        // echo "</p>";  
       
-        echo "<p>Password: ";  
-        echo form_password('password');  
-        echo "</p>";  
+        // echo "</p>";  
+        // echo form_submit('login_submit', 'Login');  
+        // echo "</p>";  
       
-        echo "</p>";  
-        echo form_submit('login_submit', 'Login');  
-        echo "</p>";  
-      
-        echo form_close();  
+        // echo form_close();  
       
         ?>  
       
